ba067682707d0384e0c85e098890587aa5b134ab
guenguen97 <guenguen97@gmail.com>
2023-12-04 14:50:56 +0900

    3단계 구현

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,21 +4,29 @@ import java.util.Scanner;
 
 public class App {
     Scanner sc;
-    App(Scanner sc){
+
+    App(Scanner sc) {
         this.sc = sc;
     }
-    void run(){
+
+    void run() {
         System.out.println("==명언앱==");
-        while(true){
+
+        int lastId = 1;
+
+        while (true) {
             System.out.print("명령) ");
             String command = sc.nextLine().trim();
-            if(command.equals("종료")){
+            if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
                 System.out.print("등록) ");
                 String content = sc.nextLine().trim();
                 System.out.print("작가) ");
                 String author = sc.nextLine().trim();
+
+                System.out.println(lastId + "번 명언이 등록 되었습니다");
+                lastId++;
             }
         }
     }
